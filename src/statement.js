02fdd809c8057923123f21c5a8b6693373a5cef5
Matheus Valandro <msvalandro@gmail.com>
2021-08-04 20:44:36 -0300

    extract function

--- src/statement.js
+++ src/statement.js
@@ -1,15 +1,13 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    // exibe a linha para esta requisição
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats) \n`;
-
-    totalAmount += amountFor(perf);
   }
+
+  let totalAmount = appleSauce();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -77,6 +75,16 @@ function statement(invoice, plays) {
     }
 
     return volumeCredits;
+  }
+
+  function appleSauce() {
+    let totalAmount = 0;
+
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+
+    return totalAmount;
   }
 }
 
