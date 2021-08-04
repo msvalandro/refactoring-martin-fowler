cdaa5920349e138fd6757f79da166ecafd8d371d
Matheus Valandro <msvalandro@gmail.com>
2021-08-04 20:33:01 -0300

    change function declaration

--- src/statement.js
+++ src/statement.js
@@ -7,14 +7,14 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // exibe a linha para esta requisição
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats) \n`;
 
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -64,12 +64,12 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 }
 
