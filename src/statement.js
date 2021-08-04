dbb2f5fc6606e5e6670bcffbeb37e6b92e6fa85f
Matheus Valandro <msvalandro@gmail.com>
2021-08-04 13:09:40 -0300

    change function declaration

--- src/statement.js
+++ src/statement.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // soma créditos por volume
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -31,7 +31,7 @@ function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
 
     switch (playFor(aPerformance).type) {
