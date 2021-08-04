15172d0f11f43bde94d675233087d947a011871d
Matheus Valandro <msvalandro@gmail.com>
2021-08-04 20:27:48 -0300

    rename variable

--- src/statement.js
+++ src/statement.js
@@ -59,15 +59,15 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
 
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    result += Math.max(aPerformance.audience - 30, 0);
 
-    if ('comedy' === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
 
-    return volumeCredits;
+    return result;
   }
 }
 
