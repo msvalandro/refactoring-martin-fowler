1763f35597ec79442d500eeef2c2b3dedd1b11c7
Matheus Valandro <msvalandro@gmail.com>
2021-08-04 20:31:01 -0300

    extract function

--- src/statement.js
+++ src/statement.js
@@ -3,12 +3,6 @@ function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
@@ -68,6 +62,14 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
 
     return result;
+  }
+
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber);
   }
 }
 
